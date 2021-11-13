78e64198ff8f72d4c1480b665043ae5ec6bd82e7
aws404 <aws40404@gmail.com>
2021-11-13 21:22:43 +1100

    Fix TestMod status effect and add client display support
    - Fixed the status effect in the test mod
    - Added support for sending a vanilla status effect to the client (for display in the HUD/Inventory screen). This defaults to nothing being sent to the client

--- src/testmod/java/eu/pb4/polymertest/TestStatusEffect.java
+++ src/testmod/java/eu/pb4/polymertest/TestStatusEffect.java
@@ -1,12 +1,14 @@
 package eu.pb4.polymertest;
 
 import eu.pb4.polymer.interfaces.VirtualObject;
+import eu.pb4.polymer.interfaces.VirtualStatusEffect;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
+import net.minecraft.entity.effect.StatusEffects;
 
-public class TestStatusEffect extends StatusEffect implements VirtualObject {
+public class TestStatusEffect extends StatusEffect implements VirtualStatusEffect {
     protected TestStatusEffect() {
         super(StatusEffectCategory.BENEFICIAL, 110011);
     }
@@ -14,7 +16,17 @@ public class TestStatusEffect extends StatusEffect implements VirtualObject {
     @Override
     public void applyUpdateEffect(LivingEntity entity, int amplifier) {
         if (entity.getMainHandStack().isDamageable()) {
-            entity.getMainHandStack().damage(1, entity, entity1 -> entity1.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND));
+            entity.getMainHandStack().damage(amplifier + 1, entity, entity1 -> entity1.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND));
         }
+    }
+
+    @Override
+    public boolean canApplyUpdateEffect(int duration, int amplifier) {
+        return true;
+    }
+
+    @Override
+    public StatusEffect getVirtualStatusEffect() {
+        return StatusEffects.CONDUIT_POWER;
     }
 }
