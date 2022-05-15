968368e35d5bfa5ab0ab102500ec45d29763aa29
Patbox <39821509+Patbox@users.noreply.github.com>
2022-05-15 20:01:19 +0200

    Fix Immersive Portals compat

--- polymer/src/main/java/eu/pb4/polymer/mixin/compat/immersive_portals/ip_ServerPlayNetworkHandlerMixin.java
+++ polymer/src/main/java/eu/pb4/polymer/mixin/compat/immersive_portals/ip_ServerPlayNetworkHandlerMixin.java
@@ -12,9 +12,7 @@ import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import qouteall.imm_ptl.core.network.IPCommonNetwork;
 import qouteall.imm_ptl.core.platform_specific.IPNetworking;
 
@@ -29,18 +27,19 @@ public abstract class ip_ServerPlayNetworkHandlerMixin {
     @Shadow
     public abstract void sendPacket(Packet<?> packet, @Nullable GenericFutureListener<? extends Future<? super Void>> listener);
 
-    @Inject(method = "sendPacket(Lnet/minecraft/network/Packet;Lio/netty/util/concurrent/GenericFutureListener;)V", at = @At("HEAD"), cancellable = true)
-    private void polymer_ip_catchAndReplace(Packet<?> packet, @Nullable GenericFutureListener<? extends Future<? super Void>> listener, CallbackInfo ci) {
+    @ModifyVariable(method = "sendPacket(Lnet/minecraft/network/Packet;Lio/netty/util/concurrent/GenericFutureListener;)V", at = @At("HEAD"))
+    private Packet<?> polymer_ip_catchAndReplace(Packet<?> packet) {
         if (packet instanceof IPAttachedPacket attachedPacket && !attachedPacket.polymer_ip_shouldSkip()
                 && attachedPacket.polymer_ip_getAttachedPacket() != null && attachedPacket.polymer_ip_getAttachedDimension() != null
         ) {
-            PolymerImplUtils.setPlayer(this.getPlayer());
+            PolymerImplUtils.setPlayer(this.player);
             var newPacket = IPNetworking.createRedirectedMessage(attachedPacket.polymer_ip_getAttachedDimension(), attachedPacket.polymer_ip_getAttachedPacket());
             ((IPAttachedPacket) newPacket).polymer_ip_setSkip(true);
-            this.sendPacket(newPacket, listener);
             PolymerImplUtils.setPlayer(null);
-            ci.cancel();
+            return newPacket;
         }
+
+        return packet;
     }
 
     @ModifyVariable(method = "sendPacket(Lnet/minecraft/network/Packet;Lio/netty/util/concurrent/GenericFutureListener;)V", at = @At("TAIL"))
