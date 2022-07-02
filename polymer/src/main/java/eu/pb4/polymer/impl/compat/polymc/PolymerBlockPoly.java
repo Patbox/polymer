53c7b82b2bab4258bf15419abaef6808428b131f
TheEpicBlock <git.teb@theepicblock.nl>
2022-07-02 19:25:20 +0200

    Respect PolyMap in PolymerBlockPoly

--- polymer/src/main/java/eu/pb4/polymer/impl/compat/polymc/PolymerBlockPoly.java
+++ polymer/src/main/java/eu/pb4/polymer/impl/compat/polymc/PolymerBlockPoly.java
@@ -5,6 +5,7 @@ import eu.pb4.polymer.api.block.PolymerBlockUtils;
 import eu.pb4.polymer.api.utils.PolymerUtils;
 import io.github.theepicblock.polymc.PolyMc;
 import io.github.theepicblock.polymc.api.block.BlockPoly;
+import io.github.theepicblock.polymc.impl.Util;
 import net.minecraft.block.BlockState;
 import org.jetbrains.annotations.ApiStatus;
 
@@ -13,6 +14,7 @@ import org.jetbrains.annotations.ApiStatus;
 public class PolymerBlockPoly implements BlockPoly {
     @Override
     public BlockState getClientBlock(BlockState input) {
-        return PolyMc.getMainMap().getClientState(PolymerBlockUtils.getPolymerBlockState(input, PolymerUtils.getPlayer()), PolymerUtils.getPlayer());
+        var polyMap = Util.tryGetPolyMap(PolymerUtils.getPlayer());
+        return polyMap.getClientState(PolymerBlockUtils.getPolymerBlockState(input, PolymerUtils.getPlayer()), PolymerUtils.getPlayer());
     }
 }
