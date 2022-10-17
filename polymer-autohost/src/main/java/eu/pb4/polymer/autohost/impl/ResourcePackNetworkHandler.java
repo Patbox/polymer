646eb95a155aecc672647cfc6233480a3d853483
techno-sam <77073745+techno-sam@users.noreply.github.com>
2022-10-17 06:43:54 -0700

    Improve AutoHost Reliability (#44)

--- polymer-autohost/src/main/java/eu/pb4/polymer/autohost/impl/ResourcePackNetworkHandler.java
+++ polymer-autohost/src/main/java/eu/pb4/polymer/autohost/impl/ResourcePackNetworkHandler.java
@@ -67,7 +67,6 @@ public class ResourcePackNetworkHandler extends EarlyPlayNetworkHandler {
     @Override
     public void onPong(PlayPongC2SPacket packet) {
         if (packet.getParameter() == 0) {
-            this.sendKeepAlive();
             this.continueJoining();
         }
     }
