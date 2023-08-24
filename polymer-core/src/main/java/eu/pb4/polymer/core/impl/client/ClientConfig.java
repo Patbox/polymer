3015ce726ffa9c7094e44b1bf89575d4fc630636
Patbox <39821509+Patbox@users.noreply.github.com>
2023-08-24 22:07:32 +0200

    Change default logging

--- polymer-core/src/main/java/eu/pb4/polymer/core/impl/client/ClientConfig.java
+++ polymer-core/src/main/java/eu/pb4/polymer/core/impl/client/ClientConfig.java
@@ -8,7 +8,7 @@ public class ClientConfig {
     public String _c4 = "Toggles visibility of F3 debug info";
     public boolean displayF3Info = true;
     public String _c5 = "Enables logging of invalid registry ids (BlockStates, Blocks, Items, etc) sent by server";
-    public boolean logInvalidServerEntryIds = true;
+    public boolean logInvalidServerEntryIds = false;
     public String _c6 = "Disables Polymer's QoL changes that effects non-visual things";
     public boolean disableNonVisualQualityOfLifeChanges = PolymerImpl.DEV_ENV;
     public String _c7 = "Enables experimental support for less standard modded containers, allowing them to display polymer items";
